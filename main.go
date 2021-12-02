9214da6620ec558cd8d041b2295e53afd005133a
jinjio <35422448+jinjio@users.noreply.github.com>
2021-12-02 15:25:41 +0900

    .

--- main.go
+++ main.go
@@ -2,10 +2,34 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/jinjio/learngo/mydict"
 )
 
 func main() {
-	a := 2
-	b := &a
-	fmt.Println(*b)
+	dictionary := mydict.Dictionary{}
+	baseWord := "hello"
+	dictionary.Add(baseWord, "First")
+	err := dictionary.Update("tt", "Second")
+	if err != nil {
+		fmt.Println(err)
+	}
+	word, _ := dictionary.Search(baseWord)
+	fmt.Println(word)
 }
+
+// import (
+// 	"fmt"
+
+// 	"github.com/jinjio/learngo/accounts"
+// )
+
+// func main() {
+// 	account := accounts.NewAccount("jio")
+// 	account.Deposit(10)
+// 	err := account.Withdraw(20)
+// 	if err != nil {
+// 		fmt.Println(err)
+// 	}
+// 	fmt.Println(account.Balance(), account.Owner())
+// }
