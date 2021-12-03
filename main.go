8454426dc52410395610ff945b99c3e03ca9dd0b
jinjio <35422448+jinjio@users.noreply.github.com>
2021-12-03 14:19:21 +0900

    .

--- main.go
+++ main.go
@@ -1,40 +1,23 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"net/http"
+	"time"
 )
 
-var errRequestFailed = errors.New("request failed")
-
 func main() {
-	var results = map[string]string{}
-	urls := []string{
-		"https://www.google.com/",
-		"https://www.naver.com/",
-		"https://www.facebook.com/",
+	c := make(chan string)
+	people := [4]string{"nico", "jio", "dal", "larry"}
+	for _, person := range people {
+		go isSexy(person, c)
 	}
-	results["gello"] = "hello"
-	for _, url := range urls {
-		result := "OK"
-		err := hitURL(url)
-		if err != nil {
-			result = "FAILED"
-		}
-		results[url] = result
-	}
-	for url, result := range results {
-		fmt.Println(url, result)
+	for i := 0; i < len(people); i++ {
+		fmt.Println("wating for", i)
+		fmt.Println(<-c)
 	}
 }
 
-func hitURL(url string) error {
-	fmt.Println("Checking:", url)
-	resp, err := http.Get(url)
-	if err != nil || resp.StatusCode >= 400 {
-		fmt.Println(err, resp.StatusCode)
-		return errRequestFailed
-	}
-	return nil
+func isSexy(person string, c chan string) {
+	time.Sleep(time.Second * 5)
+	c <- person + " is sexy"
 }
